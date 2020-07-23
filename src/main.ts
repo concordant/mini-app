21e0c2a8e9c62912800b797c70b699ecfa21812d
bieniusa <bieniusa@cs.uni-kl.de>
2020-07-24 00:16:59 +0200

    Removing non-crdt counter
    Making the app simpler again

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,13 @@ import crdtlib = require('c-crdtlib');
 let crdtl = crdtlib.crdtlib;
 
 let env = crdtl.utils.SimpleEnvironment_init_yu9ib0$("myClientId");
-
 let cntr = crdtl.crdt.PNCounter_init_u1d9q$();
-let x:number = 0;
 
 // labels
-var cntlabel_local = document.getElementById('cnt_local'); // find the HTML element in the DOM
 var cntlabel_crdt = document.getElementById('cnt_crdt'); // find the HTML element in the DOM
 
 export function incrLabel() {
     // this function is executed whenever the user clicks the increment button
-    if (cntlabel_local) {
-      cntlabel_local.textContent = (++x).toString()
-    }
     if (cntlabel_crdt) {
         var delta = cntr.increment(1, env.getNewTimestamp());
         cntr.merge(delta);
@@ -24,9 +18,7 @@ export function incrLabel() {
 
 export function decrLabel() {
     // this function is executed whenever the user clicks the decrement button
-    if (cntlabel_local)
-      cntlabel_local.textContent = (--x).toString()
-    if (cntlabel_crdt) {
+   if (cntlabel_crdt) {
         var delta = cntr.decrement(1, env.getNewTimestamp());
         cntr.merge(delta);
         cntlabel_crdt.textContent = cntr.get().toString()

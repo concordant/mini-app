f3e7f495aabc46ebdd3044331544a18bf2408e78
bieniusa <bieniusa@cs.uni-kl.de>
2020-07-23 14:29:19 +0200

    Added CRDT lib to build
    - Integration of c-crdtlib in client-code
    - TS-> JS translation and bundling with browsify automazed in build script
    - Added README with initial instructions
    
    Hint:
    - The increment and decrement of local counter with the buttons works, the crdt counter is not working correctly yet.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
-var x = 0;
+import crdtlib = require('c-crdtlib');
+var crdtl = crdtlib.crdtlib;
 
-var span = document.querySelector('span'); // find the <span> element in the DOM
+var cntr = new crdtl.crdt.PNCounter();
+var x:number = 0;
 
-function incrLabel() {
-  if (span)
-      // this function is executed whenever the user clicks the increment button
-      span.textContent = (++x).toString()
+// labels
+var cntlabel_local = document.getElementById('cnt_local'); // find the HTML element in the DOM
+var cntlabel_crdt = document.getElementById('cnt_crdt'); // find the HTML element in the DOM
+
+export function incrLabel() {
+    // this function is executed whenever the user clicks the increment button
+    if (cntlabel_local) {
+      cntlabel_local.textContent = (++x).toString()
+    }
+    if (cntlabel_crdt) {
+        cntr.increment();
+        cntlabel_crdt.textContent = cntr.get().toString()
+    }
 }
 
-function decrLabel() {
-    if (span)
-      // this function is executed whenever the user clicks the decrement button
-      span.textContent = (--x).toString()
+export function decrLabel() {
+    // this function is executed whenever the user clicks the increment button
+    if (cntlabel_local)
+      cntlabel_local.textContent = (--x).toString()
+    if (cntlabel_crdt) {
+        cntr.decrement();
+        cntlabel_crdt.textContent = cntr.get().toString()
+    }
 }
